56d0eacd39217e7d7f551a9c8a119cfc3842f268
LiviuSilaghi <szilagyi.liviu@gmail.com>
2025-05-19 13:53:28 +0100

    Api integration 1st commit

--- script.js
+++ script.js
@@ -3,19 +3,32 @@
 const weatherform = document.querySelector(".weatherform");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apikey = "c43e8a6cc6bbaf782777b666ddedee58";
+const apiKey = "c43e8a6cc6bbaf782777b666ddedee58";
 
-weatherform.addEventListener("submit", (event) => {
+weatherform.addEventListener("submit", async (event) => {
   event.preventDefault();
   const city = cityInput.value;
 
   if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayweatherinfo(weatherData);
+    } catch (error) {
+      console.error(error);
+      displayError(error);
+    }
   } else {
     displayError("Please enter a city name");
   }
 });
 
-async function getWeatherData(city) {}
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=${city}&appid=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+
+  console.log(response);
+}
 
 function displayweatherinfo(data) {}
 
@@ -25,4 +38,8 @@ function displayError(message) {
   const errorDisplay = document.createElement("p");
   errorDisplay.textContent = message;
   errorDisplay.classList.add("errorDisplay");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
 }
